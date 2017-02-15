acf48bc1deb8b5218e20c70f34bdf2be2e6768fe
Kenji Hirabayashi <mkhira2@gmail.com>
2017-02-14 21:05:50 -0600

    commented

--- main.js
+++ main.js
@@ -5,49 +5,49 @@ document.querySelector("#thanks button").addEventListener('click', function() {
 
 document.querySelector("#double button").addEventListener('click', function() {
     // TASK #2
-    var pNode = document.querySelector('#compoundInvestment')
-    var doubleUp = parseInt(pNode.innerHTML)
-    doubleUp = doubleUp += doubleUp
-    pNode.innerHTML = doubleUp
+    var pNode = document.querySelector('#compoundInvestment')   // create variable for paragraph node
+    var doubleUp = parseInt(pNode.innerHTML)                    // create variable for paragraph text
+    doubleUp = doubleUp += doubleUp                             // make text variable double itself
+    pNode.innerHTML = doubleUp                                  // make text double itself on click
 
 })
 
 
 document.querySelector("#color-circle button").addEventListener('click', function() {
     // TASK #3
-    var circleNode = document.querySelector('#circle-bw')
-    if (circleNode.style.background != 'black') {
-        return circleNode.style.background = 'black'
+    var circleNode = document.querySelector('#circle-bw')   // create variable for circle
+    if (circleNode.style.background != 'black') {           // if circle isn't black (aka is white),
+        return circleNode.style.background = 'black'        // turn it black
     }
-    if (circleNode.style.background = 'black') {
-        return circleNode.style.background = 'white'
+    if (circleNode.style.background = 'black') {            // if circle is black,
+        return circleNode.style.background = 'white'        // turn it white
     }
 
 })
 
 document.querySelector("#blow-up button").addEventListener('click', function() {
     // TASK #4}
-    var redCircle = document.querySelector('.circle-red')
-    var styleObj = window.getComputedStyle(redCircle)
-    var heightInt = parseInt(styleObj.height)
-    var widthInt = parseInt(styleObj.width)
-    if (heightInt <= 320 && widthInt <= 320) {
-        redCircle.style.height = (heightInt * 2) + 'px'
-        redCircle.style.width = (widthInt * 2) + 'px'
+    var redCircle = document.querySelector('.circle-red')   // create variable for red circle
+    var styleObj = window.getComputedStyle(redCircle)       // create avenue to access css
+    var heightInt = parseInt(styleObj.height)               // turn height into an integer
+    var widthInt = parseInt(styleObj.width)                 // turn width into an integer
+    if (heightInt <= 320 && widthInt <= 320) {              // if the height/width is < 320...
+        redCircle.style.height = (heightInt * 2) + 'px'     // double height on click &
+        redCircle.style.width = (widthInt * 2) + 'px'       // double width on click
     } else {
-        redCircle.style.height = '40px'
-        redCircle.style.width = '40px'
+        redCircle.style.height = '40px'                     // otherwise, turn back to original size
+        redCircle.style.width = '40px'                      // see above
     }
 
 })
 
 document.querySelector("#remove button").addEventListener('click', function() {
     // TASK #5
-    var ulNode = document.querySelector('#userList')
-    var liNode = ulNode.children
-    for (var i = liNode.length - 1; i >= 0; i--) {
-        if (liNode[i].classList.contains('inactive')) {
-            ulNode.removeChild(liNode[i])
+    var ulNode = document.querySelector('#userList')        // create user list node
+    var liNode = ulNode.children                            // create list item node
+    for (var i = liNode.length - 1; i >= 0; i--) {          // iterate through list items
+        if (liNode[i].classList.contains('inactive')) {     // if list item has class 'inactive'...
+            ulNode.removeChild(liNode[i])                   // ...remove it from the user list
         }
     }
 
@@ -56,25 +56,19 @@ document.querySelector("#remove button").addEventListener('click', function() {
 
 document.querySelector("#reverse-squares button").addEventListener('click', function() {
     // TASK #6
-    var squareNode = document.querySelector('#reverse-squares .answer-box')
-    var eachSquaresNode = squareNode.children
-    for (var i = eachSquaresNode.length - 1; i >= 0; i--) {
-        squareNode.appendChild(eachSquaresNode[i])
+    var squareNode = document.querySelector('#reverse-squares .answer-box') // set variable for square container
+    var eachSquaresNode = squareNode.children                               // set variable for each square
+    for (var i = eachSquaresNode.length - 1; i >= 0; i--) {                 // iterate through each square backwards
+        squareNode.appendChild(eachSquaresNode[i])                          // for each index, add to square container
     }
-
-    //     var squareNode = document.querySelector('#reverse-squares .answer-box') 
-    //     var smallSquaresNodes = document.querySelectorAll('.answer-box span') 
-    //     for(var i = smallSquaresNodes.length-1; i >=0; i--) { 
-    //         squareNode.appendChild(smallSquaresNodes[i])
-    // } 
 })
 
 document.querySelector("#pig-latin button").addEventListener('click', function() {
     // TASK #7
-    var ulNode = document.querySelector('#tasks')
-    var liNode = ulNode.children
-    for (var i = 0; i < liNode.length; i++) {
-        liNode[i].innerHTML = liNode[i].innerHTML.split('').reverse().join('')
+    var ulNode = document.querySelector('#tasks')                               // set variable for task list
+    var liNode = ulNode.children                                                // set variable for list items
+    for (var i = 0; i < liNode.length; i++) {                                   // iterate through list items
+        liNode[i].innerHTML = liNode[i].innerHTML.split('').reverse().join('')  // reverse each character in text
     }
 })
 
